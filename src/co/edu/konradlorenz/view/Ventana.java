5e8ee8a52b9647e417ab698e34f1ce971c7e36a1
Lina Bello <bellolina011@gmail.com>
2025-04-24 11:07:25 -0500

    Se agregaron try- catch en la ventana

--- src/co/edu/konradlorenz/view/Ventana.java
+++ src/co/edu/konradlorenz/view/Ventana.java
@@ -1,30 +1,64 @@
 package co.edu.konradlorenz.view;
 
 import co.edu.konradlorenz.model.*;
+import java.util.*;
 import java.util.Scanner;
 
 public class Ventana {
+
+    private Scanner scanner = new Scanner(System.in);
+
     public static void mostrarMensaje(String mensaje) {
         System.out.println(mensaje);
     }
 
     public static int ingresarDatoInt() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        int dato = 0;
+        try {
+            Ventana v = new Ventana();
+            dato = v.scanner.nextInt();
+        } catch (InputMismatchException e) {
+            mostrarMensaje("Tipo de dato nulo");
+        } catch (Exception e) {
+            mostrarMensaje("Error al guardar la variable tipo entero");
+        }
+        return dato;
+
     }
 
     public static String ingresarDatoString() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
+        String dato = null;
+        try {
+            Ventana v = new Ventana();
+            dato = v.scanner.nextLine();
+        } catch (NullPointerException e) {
+            mostrarMensaje("Tipo de dato nulo");
+        } catch (Exception e) {
+            mostrarMensaje("Error al guardar la variable String");
+        }
+        return dato;
     }
 
     public static Prioridad ingresarDatoPrioridad() {
-        Scanner scanner = new Scanner(System.in);
+        Ventana v = new Ventana();
         for (Prioridad prioridad : Prioridad.values()) {
             System.out.println(prioridad);
         }
-        String dato = scanner.nextLine().toUpperCase();
-        return Prioridad.valueOf(dato);
+        String dato = v.scanner.nextLine().toUpperCase();
+        Prioridad prioridad = Prioridad.SIN_PRIORIDAD;
+        try {
+            prioridad = Prioridad.valueOf(dato);
+        } catch (IllegalArgumentException e) {
+            mostrarMensaje("Valor no valido para enum prioridad");
+        } catch (NullPointerException e) {
+            mostrarMensaje("El valor ingresado es nulo");
+        } catch (Exception e) {
+            mostrarMensaje("Error desconocido al guardadr valor de enum prioridad");
+        } finally {
+            mostrarMensaje("Prioridad guardada como: " + prioridad);
+        }
+
+        return prioridad;
     }
 
     public static int mostrarMenuPrincipal() {
