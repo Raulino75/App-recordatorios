e455e6adf6cd49c06c64b2444041916054640535
Raul Arturo Pardo <raularturo75@gmail.com>
2025-05-23 03:33:57 -0500

    Auto stash before merge of "main" and "origin/main"

--- src/co/edu/konradlorenz/controller/AplMain.java
+++ src/co/edu/konradlorenz/controller/AplMain.java
@@ -3,6 +3,13 @@ package co.edu.konradlorenz.controller;
 public class AplMain {
 
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         new Controller().run();
+=======
+        new Controlador().run();
+        System.out.println("EEEEEEEEEEEEEEEASDASDASDADSDADS");
+        System.out.println(""
+                + "so");
+>>>>>>> Stashed changes
     }
 }
