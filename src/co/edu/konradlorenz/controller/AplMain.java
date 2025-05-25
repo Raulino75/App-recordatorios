6ec60fa721807e5305c5994aa38db84ab9d1ae85
Lina Bello <bellolina011@gmail.com>
2025-05-24 19:48:18 -0500

    APLMAIN TEMPORAL

--- src/co/edu/konradlorenz/controller/AplMain.java
+++ src/co/edu/konradlorenz/controller/AplMain.java
@@ -1,8 +1,16 @@
 package co.edu.konradlorenz.controller;
 
+import co.edu.konradlorenz.view.Home;
+
 public class AplMain {
 
     public static void main(String[] args) {
-        new Controller().run();
+        // new Controller().run();
+        
+        //TODO: BORRAR DE LA 11 A LA 14 cuando el controller esté listo
+        Controller ctrl = new Controller();
+        Home home = new Home(ctrl);
+        home.setLocationRelativeTo(null);
+        home.setVisible(true);
     }
 }
